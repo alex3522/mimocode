7d070adaf692c5a9700cc7974fd32a0ecf7abf30
Abez <alexberry80@gmail.com>
2025-04-02 12:58:26 +0100

    1c-wrap_aligncontent

--- javascript/flexboxscript.js
+++ javascript/flexboxscript.js
@@ -42,3 +42,39 @@ function flexRowReverse() {
     const container = document.querySelector('.container');
     container.style.flexDirection = 'row-reverse';
 }
+function wrap() {
+    const container = document.querySelector('.container');
+    container.style.flexWrap = 'wrap';
+}
+function noWrap() {
+    const container = document.querySelector('.container');
+    container.style.flexWrap = 'nowrap';
+}
+function wrapReverse() {
+    const container = document.querySelector('.container');
+    container.style.flexWrap = 'wrap-reverse';
+}
+function alignContentCenter() {
+    const container = document.querySelector('.container');
+    container.style.alignContent = 'center';
+}
+function alignContentStart() {
+    const container = document.querySelector('.container');
+    container.style.alignContent = 'flex-start';
+}
+function alignContentEnd() {
+    const container = document.querySelector('.container');
+    container.style.alignContent = 'flex-end';
+}
+function alignContentSpaceAround() {
+    const container = document.querySelector('.container');
+    container.style.alignContent = 'space-around';
+}
+function alignContentSpaceBetween() {
+    const container = document.querySelector('.container');
+    container.style.alignContent = 'space-between';
+}
+function alignContentStretch() {
+    const container = document.querySelector('.container');
+    container.style.alignContent = 'stretch';
+}
